a5b87348423958449a03a97c1cd60d708ad89208
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-10-04 10:37:16 +0100

    imported API and pass props down and imported stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/list.jsx';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      reviews: []
+    };
+  }
+
+
+  componentDidMount(){
+    const url = "http://localhost:3000/"
+    fetch(url)
+    .then((response) => response.json())
+    .then((response) => this.setState( { reviews: response} ) );
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -11,7 +28,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <List
+        reviews={this.state.reviews} />
         </p>
       </div>
     );
--- frontend/src/components/list.jsx
+++ frontend/src/components/list.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import Detail from './detail.jsx';
 
 class List extends React.Component{
 
     render(){
 
-        const details = this.props.data.map((object, index) => {
-            return <Detail/>
+        const details = this.props.reviews.map((review, index) => {
+            return <Detail id={index} details={review}/>
         })
 
         return(
             <section>
-
                {details} 
-
             </section>
         )
     }
