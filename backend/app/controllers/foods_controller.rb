9bc1e9bd9cbd061683722480bff305026923bf3a
Matthew Shield <matthewshield@hotmail.co.uk>
2017-10-04 10:08:52 +0100

    Changed seeds and controller to reflect new food params

--- backend/app/controllers/foods_controller.rb
+++ backend/app/controllers/foods_controller.rb
@@ -1,39 +1,40 @@
 class FoodsController < ApplicationController
-
-  def food_params
-    params.require(:food).permit([:name, :c_rating, :m_rating, :r_rating])
-  end
-
-  def index
-    foods = Food.all
-    render :json => foods
-  end
-
-  def show
-    food = Food.find(params[:id])
-    render :json => food
-  end
-
-  def create
-    food = Food.create(food_params)
-    render :json => food
-  end
-
-  def destroy
-    food = Food.find(params[:id])
-    if food.destroy!
-      render :json => {status: :success}
-    else
-      render :json => {status: :failed}
+  
+    def food_params
+      params.require(:food).permit([:name, :rating, :author, :comment])
     end
-  end
-
-  def update
-    food = Food.find(params[:id])
-    if food.update_attributes(food_params)
+  
+    def index
+      foods = Food.all
+      render :json => foods
+    end
+  
+    def show
+      food = Food.find(params[:id])
       render :json => food
-    else
-      render :json => {status: :update_failed}
+    end
+  
+    def create
+      food = Food.create(food_params)
+      render :json => food
+    end
+  
+    def destroy
+      food = Food.find(params[:id])
+      if food.destroy!
+        render :json => {status: :success}
+      else
+        render :json => {status: :failed}
+      end
+    end
+  
+    def update
+      food = Food.find(params[:id])
+      if food.update_attributes(food_params)
+        render :json => food
+      else
+        render :json => {status: :update_failed}
+      end
     end
   end
-end
+  
